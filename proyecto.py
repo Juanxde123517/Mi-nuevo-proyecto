c01e123023c4155ceaa78e57e1d982250e1d82e4
Juan <Juanfrarela2007@gmail.com>
2026-08-26 13:04:02 -0500

    Clase usuario

--- proyecto.py
+++ proyecto.py
@@ -16,3 +16,21 @@ class Tarea:
         self.nuevo_titulo = nuevo_titulo
         self.nueva_descripcion = nueva_descripcion
         self.fecha_vencimiento = nueva_fecha
+
+#Class Usuario
+class Usuario:
+    def __init__(self, nombre_usuario, contraseña):
+        self.nombre_usuario = nombre_usuario
+        self.nombre_usuario = contraseña
+        self.tareas = []
+
+    def agregar_tareas(self, tarea):
+        self.tareas.append(tarea)
+
+    def eliminar_tarea(self, titulo_tarea):
+        self.tareas = [tarea for tarea in self.tareas if tarea.titulo != titulo_tarea]
+        #Recorre toda la lista de tareas hasta llegar a la tarea que tenga el titulo igual al ingresado
+
+    def obtener_tareas(Self):
+        return self.tareas
+        

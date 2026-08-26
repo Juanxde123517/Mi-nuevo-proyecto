1ae9ace59e5a4ceddba9be2b3ae5f2d40b9595cd
Juan <Juanfrarela2007@gmail.com>
2026-08-26 12:52:24 -0500

    Clase Tareas

---
+++ proyecto.py
@@ -0,0 +1,18 @@
+import json
+from datetime import datetime
+
+#Class Tarea
+class Tarea:
+    def __init__(self, titulo, descripcion, fecha_vencimiento):
+        self.titulo = titulo
+        self.descripcion = descripcion
+        self.fecha_vencimiento= fecha_vencimiento
+        self.completado = False #Hasta que nosotros no le demos una indicación de que está completado, debe mantenerse en falso (sin completar)
+
+    def marcar_completada(self):
+        self.completado = True
+
+    def editar_tarea(self, nuevo_titulo, nueva_descripcion, nueva_fecha):
+        self.nuevo_titulo = nuevo_titulo
+        self.nueva_descripcion = nueva_descripcion
+        self.nueva_fecha = nueva_fecha
